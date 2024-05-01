7498a05d0fb7ef5de9c7089a952fe1cf5e3e9265
Iommy <iommy_dev@mail.ru>
2024-05-01 15:05:52 +0300

    Обновление

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 
 import './App.css';
 import Auth from "./pages/auth/Auth.tsx";
+import {
+    WindowCenter,
+    WindowSetSize
+} from "../wailsjs/runtime/runtime.js";
 
 
 function App() {
+
+    WindowSetSize(1100, 700)
+    WindowCenter()
 
     return (
         <>
--- frontend/src/pages/auth/Auth.tsx
+++ frontend/src/pages/auth/Auth.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import s from './auth.module.css'
 import Input from "../../components/UI/Input/Input";
+import logo from '../../../../build/appicon.png'
+
 
 const Auth = () => {
+
+
+
+
     return (
-        <div>
+        <div className={s.container}>
+            <div className={s.logo}>
+            <img src={logo}/>
+            </div>
+
+            <div>
             <h1 className={s.auth_title}>Авторизация</h1>
             <div className={s.wrapper}>
                 <Input placeholder='Введите никнейм'/>
                 <Input placeholder='Введите пароль'/>
             </div>
             <button className={s.btn}>Войти в аккаунт</button>
+            </div>
         </div>
     );
 };
